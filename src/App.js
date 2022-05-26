bd65ce1744dd4b4a0a8f68e6f9c0f6f05e8e36b9
Tanvi Patil <tanvipatil@Tanvis-MacBook-Pro.local>
2022-05-25 21:10:44 -0400

    redirects

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 
 
 import Welcome from './pages/Welcome';
@@ -12,9 +12,10 @@ function App() {
       <MainHeader/>
       <main>
         <Routes>
-          <Route path="/welcome" element={<Welcome/>}/>
+          <Route path="/" element={<Navigate to="/welcome"/>}/>
+          <Route path="/welcome/*" element={<Welcome/>}/>
           <Route path="/products" element={<Products/>}/>
-          <Route path="/product-detail/:productId" element={<ProductDetail/>}/>
+          <Route path="/products/:productId" element={<ProductDetail/>}/>
         </Routes>
       </main>
     </div>
--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,5 +1,12 @@
+import {Routes, Route} from 'react-router-dom'
+
 const Welcome = () => {
-    return <h1>The welcome page</h1>;
+    return (<section>
+    <h1>The welcome page</h1>
+    <Routes>
+          <Route path="/user" element={<p>user</p>}/>
+    </Routes>
+    </section>);
 };
 
 export default Welcome;
